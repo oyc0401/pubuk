ee345c835384f256873ae58e87fd1e107fede333
oyc0401 <oyc0401@gmail.com>
2022-05-15 15:30:03 +0900

    모든 사람을 대상으로 하는 앱 먼저 만들어보기

---
+++ lib/page/Profile/signup.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({Key? key}) : super(key: key);
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
