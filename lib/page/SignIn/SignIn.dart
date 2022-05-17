ba5b6265adfc261861e62da93df128010e497b64
oyc0401 <oyc0401@gmail.com>
2022-05-18 01:40:58 +0900

    구글 로그인 처음 로그인 화면 만듬

--- lib/page/SignIn/SignIn.dart
+++ lib/page/SignIn/SignIn.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterschool/page/Home/home.dart';
-import 'package:flutterschool/page/SignIn/register.dart';
+
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'dart:async';
+import 'dart:convert' show json;
+
+import 'package:http/http.dart' as http;
+
+import '../Home/home.dart';
+import 'register.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  clientId:
+      '38235317642-4i8ul6m33g6ljpap0bk2b46lv05k1j79.apps.googleusercontent.com',
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -11,6 +29,26 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  GoogleSignInAccount? _currentUser;
+  String _contactText = '';
+
+
+  @override
+  void initState() {
+    super.initState();
+    /// 테스트기간에만 주석 해놓는것이다. 이 페이지에 들어오면 무조건 로그인 해제 상태여야 한다.
+    //_handleSignOut();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+        _currentUser = account;
+        if(account!=null){
+          print(account.toString());
+        }
+          print("로그인?");
+          navigate(account);
+    });
+    _googleSignIn.signInSilently();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -86,10 +124,19 @@ class _SignInState extends State<SignIn> {
     );
   }
 
-  GoogleLogin() async {
-    bool isExisted=await login();
-    
-    if(isExisted){
+  Future<void> GoogleLogin() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+  Future<void> _handleSignOut() => _googleSignIn.disconnect();
+
+
+  void navigate(GoogleSignInAccount? user){
+    if (user != null) {
+      print("유저 정보가 있습니다.");
       Navigator.pushReplacement(
         context,
         CupertinoPageRoute(
@@ -97,18 +144,14 @@ class _SignInState extends State<SignIn> {
         ),
       );
     }else{
-      Navigator.pushReplacement(
-        context,
-        CupertinoPageRoute(
-          builder: (context) => const MyHomePage(title: "학교"),
-        ),
-      );
+      print("유저는 현재 비 로그인 상태이다.");
     }
-    
-  }
 
-  login()async{
-
-    return false;
+    //       Navigator.pushReplacement(
+        //     context,
+        //     CupertinoPageRoute(
+        //       builder: (context) => const MyHomePage(title: "학교"),
+        //     ),
+        //   );
   }
 }
--- lib/page/SignIn/register.dart
+++ lib/page/SignIn/register.dart
@@ -1,4 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  clientId:
+      '38235317642-4i8ul6m33g6ljpap0bk2b46lv05k1j79.apps.googleusercontent.com',
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
 
 class register extends StatefulWidget {
   const register({Key? key}) : super(key: key);
@@ -12,7 +24,16 @@ class _registerState extends State<register> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: Column(
+        children: [
+          CupertinoButton(
+            child: Text("jjj"),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
     );
   }
+
 }
