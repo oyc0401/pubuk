5b33a75c44cec6bfe8343e60a9688fb27cd6ee58
oyuchan <oyc0401@gmail.com>
2022-08-23 02:43:56 +0900

    대충 ui 잡음

--- lib/page/Home/Lunch/LunchInfo.dart
+++ lib/page/Home/Lunch/LunchInfo.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'Lunch.dart';
 
-String allergy =
-    "요리명에 표시된 번호는 알레르기를 유발할수 있는 식재료입니다 (1.난류, 2.우유, 3.메밀, 4.땅콩, 5.대두, 6.밀, 7.고등어, 8.게, 9.새우, 10.돼지고기, 11.복숭아, 12.토마토, 13.아황산염, 14.호두, 15.닭고기, 16.쇠고기, 17.오징어, 18.조개류(굴,전복,홍합 등)";
+const String allergy = "요리명에 표시된 번호는 알레르기를 유발할수 있는 식재료입니다 "
+    "(1.난류, 2.우유, 3.메밀, 4.땅콩, 5.대두, 6.밀, 7.고등어, 8.게, 9.새우, 10.돼지고기, 11.복숭아, 12.토마토,"
+    " 13.아황산염, 14.호두, 15.닭고기, 16.쇠고기, 17.오징어, 18.조개류(굴,전복,홍합 등)";
 
 class LunchInfo extends StatelessWidget {
   LunchInfo({Key? key, required this.lunch}) : super(key: key);
@@ -18,22 +19,40 @@ class LunchInfo extends StatelessWidget {
         style: TextStyle(color: Colors.black),
       )),
       body: ListView(
+        padding: EdgeInsets.all(12.0),
         children: [
           Padding(
-            padding: const EdgeInsets.all(12.0),
+            padding: const EdgeInsets.all(8.0),
             child: menuWidget(),
           ),
+          Center(
+              child: Text(
+            "급식을 선택해주세요",
+            style: TextStyle(fontSize: 24),
+          )),
+          Container(
+            height: 200,
+            margin: EdgeInsets.all(8.0),
+            color: Colors.grey,
+            child: Center(
+                child: Text(
+              "아직 개발중 입니다.",
+              style: TextStyle(color: Colors.white),
+            )),
+          ),
           Padding(
-            padding: const EdgeInsets.all(12.0),
+            padding: const EdgeInsets.all(8.0),
             child: allergyWidget(),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              nutrientWidget(),
-              originWidget(),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(child: nutrientWidget()),
+                Expanded(child: originWidget()),
+              ],
+            ),
           ),
         ],
       ),
@@ -45,7 +64,8 @@ class LunchInfo extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text("메뉴", style: TextStyle(fontSize: 28)),
-        for (String text in lunch.dish) Text(text,style: TextStyle(fontSize: 16)),
+        for (String text in lunch.dish)
+          Text(text, style: TextStyle(fontSize: 16)),
       ],
     );
   }
